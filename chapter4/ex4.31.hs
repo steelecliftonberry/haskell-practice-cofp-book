cf9331c6a9a6fe616e965c781af1ac2b4c99e418
Steele Clifton-Berry <steelecb+haskell99probs@gmail.com>
2017-12-20 17:31:28 +1100

    Exercise 4.31

---
+++ chapter4/ex4.31.hs
@@ -0,0 +1,15 @@
+import Test.QuickCheck
+
+-- Using Euclid's algorithm for GCD:
+myGcd :: Integral a => a -> a -> a
+myGcd 0 b = b
+myGcd a 0 = a
+myGcd a b = myGcd b r
+  where r = a `mod` b
+
+test1 :: Bool
+test1 = myGcd 270 192 == 6
+
+prop_answer_divides_both_inputs a b = a > 0 && b > 0 ==> (a `mod` (myGcd a b) == 0) && (b `mod` (myGcd a b) == 0)
+
+prop_gcd_positive a b = a > 0 && b > 0 ==> myGcd a b > 0
